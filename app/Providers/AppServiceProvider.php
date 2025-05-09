9506dd43a22504aae20a3df908419ddafdb89850
Issa Gomes <codedbyissa@gmail.com>
2025-05-09 16:36:05 -0300

    AppServiceProvider update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,7 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
 
-        if (env('FORCE_HTTPS') === 'true' && App::environment('production')) {
+        if (App::environment('production')) {
             URL::forceScheme('https');
         }
     }
